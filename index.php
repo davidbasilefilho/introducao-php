7e2f657da07b84c150b99965779cb2ecbc52d3cd
David Basile Filho <davidbasilefilho@gmail.com>
2023-05-07 16:34:31 -0300

    added a 404 page

--- index.php
+++ index.php
@@ -11,10 +11,12 @@
     <?php
     $page = filter_input(INPUT_GET, 'p');
 
-    if (isset($page) && !empty($page) && file_exists($page . '.php')) {
-        include_once $page . '.php';
+    if (isset($page) && !empty($page) && file_exists("{$page}.php")) {
+        include_once "{$page}.php";
+    } else if (isset($page) && !empty($page) && !file_exists($page)) {
+        include_once "404.php";
     } else {
-        include_once 'home.php';
+        include_once "home.php";
     }
     ?>
 
